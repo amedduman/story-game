7535eb1e49e4af49e9022c4afbc964d90863903e
amedduman <amedduman@protonmail.com>
2023-02-11 01:22:09 +0300

    working on completion logic

--- Assets/_Game/Scripts/RoadCompletionChecker.cs
+++ Assets/_Game/Scripts/RoadCompletionChecker.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -10,6 +12,11 @@ public class RoadCompletionChecker : MonoBehaviour
     List<RoadPiece> _roads = new List<RoadPiece>();
     List<Tile> _allTiles = new List<Tile>();
 
+    void Start()
+    {
+        _startTile.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.black;
+        _endTile.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.black;
+    }
 
     public void AddRoad(RoadPiece rp)
     {
@@ -24,24 +31,104 @@ public class RoadCompletionChecker : MonoBehaviour
         {
             _allTiles.AddRange(road.Tiles);
         }
+        
+        foreach (var allTile in _allTiles)
+        {
+            allTile.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.gray;
+        }
 
-        Check(_startTile);
+        // StartCoroutine(Check());
+        // Check(_startTile);
     }
 
-    void Check(Tile ttile)
+    IEnumerator Check()
     {
-        if (ttile == _endTile)
-        {
-            Debug.Log("complete");
-            
-        }
-        Tile currenTile = ttile;
-        List<Tile> currentNeighbours = currenTile.GetNeighbours();
-        foreach (var tile in _allTiles)
-        {
-            if (currentNeighbours.Contains(tile)) DOVirtual.DelayedCall(.1f, () => Check(tile));
-        }
+        Tile currenTile = _startTile;
 
-        Debug.Log("not complete");
+        while (currenTile != _endTile)
+        {
+            List<Tile> currentNeighbours = currenTile.GetNeighbours();
+
+            foreach (var currentNeighbour in currentNeighbours)
+            {
+                currentNeighbour.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+            }
+        
+            bool roadAvailable = false;
+        
+            foreach (var tile in _allTiles)
+            {
+                if (currentNeighbours.Contains(tile))
+                {
+                    roadAvailable = true;
+                    currenTile = tile;
+                    currenTile.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green;
+                    break;
+                } 
+            }
+            
+            if (roadAvailable)
+            {
+                Debug.Log("searching road");
+            }
+            else
+            {
+                Debug.Log("not complete");
+            }
+            Debug.Break();
+            yield return null;
+        }
     }
+
+    // int i;
+    // void Check(Tile ttile)
+    // {
+    //     i++;
+    //     if (i > 999)
+    //     {
+    //         Debug.Log("reached limit");
+    //         return;
+    //     }
+    //     Tile currenTile = ttile;
+    //
+    //     if (currenTile == _endTile)
+    //     {
+    //         Debug.Log("complete");
+    //         return;
+    //     }
+    //     List<Tile> currentNeighbours = currenTile.GetNeighbours();
+    //
+    //     foreach (var currentNeighbour in currentNeighbours)
+    //     {
+    //         currentNeighbour.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.red;
+    //     }
+    //     
+    //     foreach (var currentNeighbour in currentNeighbours)
+    //     {
+    //         currentNeighbour.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green;
+    //     }
+    //     
+    //     bool roadAvailable = false;
+    //     Tile newTile = null;
+    //     
+    //     foreach (var tile in _allTiles)
+    //     {
+    //         if (currentNeighbours.Contains(tile))
+    //         {
+    //             roadAvailable = true;
+    //             newTile = tile;
+    //             break;
+    //         } 
+    //     }
+    //
+    //     if (roadAvailable)
+    //     {
+    //         Debug.Log("searching road");
+    //         Check(newTile);
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("not complete");
+    //     }
+    // }
 }
